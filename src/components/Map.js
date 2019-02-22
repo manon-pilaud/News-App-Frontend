938d4c47db415c52d85f954b51b0045e10ddd665
Manon Pilaud <mandyypilaud@gmail.com>
2019-02-22 10:58:23 -0500

    accessing correct geolayer

--- src/components/Map.js
+++ src/components/Map.js
@@ -14,10 +14,18 @@ const linePaint: MapboxGL.LinePaint = {
   'line-width': 0.5
 };
 
-const onClickMap=(map,evt)=> {
-  const features = map.queryRenderedFeatures(evt.point, { GeoJSONLayer: ['country-layer'] })
-  let countryName = features[0].properties.name_en
-  console.log(countryName)
+const fillPaint: MapboxGL.FillPaint={
+    'fill-color': 'rgba(246,245,245,0)'
+}
+
+
+
+
+const onClickMap=(features)=> {
+  if(Array.isArray(features)){
+    let feature = features[0].properties.sovereignt
+    console.log(feature)
+  }
 }
 
 export default class WorldMap extends React.Component{
@@ -34,8 +42,13 @@ export default class WorldMap extends React.Component{
         <GeoJSONLayer
           data={'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson'}
           id='country-layer'
+          fillPaint={fillPaint}
           linePaint={linePaint}
-        />
+          fillOnClick={
+            (e)=>{
+              onClickMap(e.features)}}
+          >
+        </GeoJSONLayer>
         </Map>
 
     )
