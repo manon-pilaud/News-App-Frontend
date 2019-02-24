de9b15b4bd175a2bc3e0a93a530a8879f7966232
Manon Pilaud <mandyypilaud@gmail.com>
2019-02-24 17:02:24 -0500

    added toggle between local and regular news

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -6,7 +6,8 @@ class ArticleList extends React.Component{
   constructor(){
     super()
     this.state={
-      clicked: false
+      clicked: false,
+      toggled: false
     }
   }
 
@@ -16,21 +17,40 @@ class ArticleList extends React.Component{
     })
   }
 
+  toggled=()=>{
+    this.setState({
+      toggled: !this.state.toggled
+    })
+  }
+
   render(){
-  return !this.props.articles?null:(
+  return !this.props.articles || !this.props.localArticles?null:(
     <div className="articles-cont">
       <h3>News</h3>
       <button onClick={this.clicked}>Hide News</button>
-      <input type="checkbox" id="switch"/><label htmlFor="switch">Toggle</label>
+      <input type="checkbox" id="switch" onChange={this.toggled}/><label htmlFor="switch">Toggle</label>
       {!this.state.clicked?
-      <div>
-        {this.props.articles.map((article,index) => (
-          <ArticleCard
-            key={index}
-            articleInfo={article}
-          />
-        ))}
-      </div>:null}
+      <div className="all-articles">
+        {!this.state.toggled?
+        <div>
+          {this.props.articles.map((article,index) => (
+            <ArticleCard
+              key={index}
+              articleInfo={article}
+            />
+          ))}
+        </div>
+        :
+        <div>
+          {this.props.localArticles.map((article,index) => (
+            <ArticleCard
+              key={index}
+              articleInfo={article}
+            />
+          ))}
+        </div>}
+      </div>
+      :null}
     </div>
   )
   }
@@ -38,7 +58,8 @@ class ArticleList extends React.Component{
 
 const mapStateToProps = state => {
   return{
-    articles: state.articles.articles
+    articles: state.articles.articles,
+    localArticles: state.localArticles.articles
   }
 }
 
