b32e8d83ee944b50528dd95c029cef723aa05005
Manon Pilaud <mandyypilaud@gmail.com>
2019-02-27 21:38:24 -0500

    satalite added

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import ReactMapboxGl, {GeoJSONLayer} from "react-mapbox-gl";
+import ReactMapboxGl, {GeoJSONLayer,Feature,Layer} from "react-mapbox-gl";
 import {connect} from 'react-redux'
 import Geocoder from 'react-mapbox-gl-geocoder'
 // import {withRouter} from 'react-router-dom';
@@ -21,6 +21,14 @@ const fillPaint: MapboxGL.FillPaint={
 
 
 class WorldMap extends React.Component{
+  state={
+    mapToggle: false
+  }
+  worldToggled=()=>{
+    this.setState({
+      mapToggle: !this.state.mapToggle
+    })
+  }
   onClickMap=(features,countries)=> {
     if (Array.isArray(countries) && Array.isArray(features)){
       let countriesList = countries
@@ -35,13 +43,14 @@ class WorldMap extends React.Component{
   render(){
     return(
       <div>
+        {!this.state.mapToggle?
         <Map
           containerStyle={{ width: '100vw', height: '100vh'}}
           onClick={this.onClickMap}
           container={'map'}
           style={'mapbox://styles/mandyyp/cjsnp8ymx661o1fpl9r9l7puq'}
           center={[11.883267, 41.865919]}
-          zoom={[1.15]}
+          zoom={[1.00]}
           >
           <GeoJSONLayer
             data={'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson'}
@@ -53,7 +62,43 @@ class WorldMap extends React.Component{
                 this.onClickMap(e.features, this.props.countries)}}
             >
           </GeoJSONLayer>
-          </Map>
+          <Layer type="circle" id="marker" paint={{
+             'circle-color': "white",
+             'circle-stroke-width': 2.5,
+             'circle-stroke-color': 'white',
+             'circle-stroke-opacity': 1
+          }}>
+            <Feature onClick={this.worldToggled} coordinates={[-2.122169,81.740307]}/>
+        </Layer>
+          </Map>:
+          <Map
+            containerStyle={{ width: '100vw', height: '100vh'}}
+            onClick={this.onClickMap}
+            container={'map'}
+            style={'mapbox://styles/mandyyp/cjsmbr4cm0nue1fnue0hvno8i'}
+            center={[11.883267, 41.865919]}
+            zoom={[1.00]}
+            >
+            <GeoJSONLayer
+              data={'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_50m_admin_0_countries.geojson'}
+              id='country-layer'
+              fillPaint={fillPaint}
+              linePaint={linePaint}
+              fillOnClick={
+                (e)=>{
+                  this.onClickMap(e.features, this.props.countries)}}
+              >
+            </GeoJSONLayer>
+              <Layer type="circle" id="marker" paint={{
+                 'circle-color': "white",
+                 'circle-stroke-width': 2.5,
+                 'circle-stroke-color': 'white',
+                 'circle-stroke-opacity': 1
+              }}>
+                <Feature onClick={this.worldToggled} coordinates={[-2.122169,81.740307]}/>
+            </Layer>
+            </Map>
+          }
         </div>
 
     )
