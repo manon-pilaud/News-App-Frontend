a7f933407570e25346875204890258404329ac1a
Manon Pilaud <mandyypilaud@gmail.com>
2019-03-01 11:59:49 -0500

    displaying my countries and all countries

--- src/components/MyCountries.js
+++ src/components/MyCountries.js
@@ -2,18 +2,42 @@ import React from 'react'
 import {connect} from 'react-redux'
 import CountryCard from './CountryCard'
 class MyCountries extends React.Component{
+  state={
+    showAll: false
+  }
+
+  showAll=()=>{
+    this.setState({
+      showAll: !this.state.showAll
+    })
+  }
   render(){
-    return !this.props.userCountries?null:(
-      <div>
+    console.log(this.props.countries)
+    return(
+    <div>
+      {!this.state.showAll?
+      <div className="my-countries">
+      <button onClick={this.showAll}>Display all Countries</button>
+      {!this.props.userCountries?null:<div>
         {this.props.userCountries.map(country=><CountryCard key={country.id} country={country}/>)}
-      </div>
+      </div>}
+    </div>:
+    <div>
+    <button onClick={this.showAll}>Display My Countries</button>
+      {!this.props.countries?null:<div>
+        {this.props.countries.map(country=><CountryCard key={country.id} country={country}/>)}
+      </div>}
+    </div>
+  }
+    </div>
     )
   }
 }
 
 const mapStateToProps = (state) => {
   return{
-    userCountries: state.currentUser.countries
+    userCountries: state.currentUser.countries,
+    countries: state.countries
   }
 }
 
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,11 @@
 import React from 'react'
+let feedBBC = "http://feeds.bbci.co.uk/news/world/rss.xml"
+let feedCNN ="http://rss.cnn.com/rss/edition_world.rss"
+let feedReuters ="http://feeds.reuters.com/Reuters/worldNews"
 export default class Profile extends React.Component{
   render(){
     return(
       <div>
-        Welcome User
       </div>
     )
   }
