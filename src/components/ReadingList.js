5176cd25b7e2d4c10af74d91da770c058e3de18d
Manon Pilaud <mandyypilaud@gmail.com>
2019-03-05 19:46:26 -0500

    reading list filtered

--- src/components/ReadingList.js
+++ src/components/ReadingList.js
@@ -2,11 +2,20 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Card} from 'semantic-ui-react'
 import ReadingListCard from './ReadingListCard'
+import { clearSearch } from '../redux/actionCreator'
+import SearchBar from './CountrySearchBar'
 class ReadingList extends React.Component{
+  componentWillUnmount(){
+    this.props.clearSearch()
+  }
   render(){
     return !this.props.readingList?null:(
         <Card.Group itemsPerRow={4} id="reading-list-section">
-        {this.props.readingList.map((article,index)=><ReadingListCard key={index} articleInfo={article}/>)}
+          <SearchBar/>
+        {this.props.readingList.filter(
+          r =>
+            r.title.toLowerCase().includes(this.props.searchText.toLowerCase())
+        ).map((article,index)=><ReadingListCard key={index} articleInfo={article}/>)}
       </Card.Group>
     )
   }
@@ -14,8 +23,15 @@ class ReadingList extends React.Component{
 
 const mapStateToProps=state=>{
   return{
-    readingList: state.currentUser.articles
+    readingList: state.currentUser.articles,
+    searchText: state.filters
   }
 }
 
-export default (connect(mapStateToProps)(ReadingList))
+const mapDispatchToProps=dispatch=>{
+  return{
+      clearSearch: () => {dispatch(clearSearch())}
+  }
+}
+
+export default (connect(mapStateToProps,mapDispatchToProps)(ReadingList))
