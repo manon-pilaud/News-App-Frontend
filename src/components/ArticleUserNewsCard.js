cf19dc57f03d6e6e4ce3ed7aa646b43ce0693ffa
Manon Pilaud <mandyypilaud@gmail.com>
2019-05-22 07:11:30 -0400

    user news redux fix

--- src/components/ArticleUserNewsCard.js
+++ src/components/ArticleUserNewsCard.js
@@ -1,55 +1,8 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Card,Button} from 'semantic-ui-react'
+import {addingToReadingList} from '../redux/action/ReadingListActions'
 class ArticleUserNewsCard extends React.Component{
-//REDUX FIX- REUSE FROM ARTICLE CARD
-  createArticle=(articleInfo,country_id)=>{
-    let articleExists= this.props.savedArticles.find(savedArticle=>savedArticle.title === articleInfo.title)
-    if (!!articleExists){
-      this.addToReadingList(articleExists)
-    }
-    else{
-      fetch('http://localhost:3000/api/v1/articles',{
-       method: "POST",
-       headers:{
-         "Content-Type" : "application/json",
-         "Accept" : "application/json"
-       },
-       body: JSON.stringify({
-          title: articleInfo.title,
-          description: articleInfo.description,
-          article_url: articleInfo.url,
-          image_url: articleInfo.urlToImage,
-          country_id: country_id
-       })
-     })
-     .then(response=>response.json())
-     .then(data=>this.addToReadingList(data))
-    }
-  }
-
-  addToReadingList=(data)=>{
-    if(this.props.user.articles.find(article=> article.id === data.id)){
-      alert("Pshhht you already added this to your reading list")
-    }
-    else{
-       fetch('http://localhost:3000/api/v1/reading_lists',{
-        method: "POST",
-        headers:{
-          "Content-Type" : "application/json",
-          "Accept" : "application/json"
-        },
-        body: JSON.stringify({
-           article_id: data.id,
-           user_id: this.props.user.user_id
-        })
-      })
-      .then(response=>response.json())
-      .then(joinInfo=>this.props.updateReadingList(joinInfo))
-      .then(this.props.addToReadingList(data))
-    }
-  }
-
 
   render(props){
     let {title,content,source,url,urlToImage} = this.props.articleInfo
@@ -65,7 +18,7 @@ class ArticleUserNewsCard extends React.Component{
           </Card.Description>
         </Card.Content>
         <Card.Content extra>
-            <Button basic color='green' o onClick={()=>this.createArticle(this.props.articleInfo,this.props.country.id)}>
+            <Button basic color='green' onClick={()=>this.props.addToReadingList(this.props.articleInfo,this.props.country.id)} >
               Add to Reading List
             </Button>
         </Card.Content>
@@ -85,8 +38,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps=dispatch=>{
   return{
-    updateReadingList:(joinInfo)=>{dispatch({type:"UPDATE_READING_LIST",joinInfo})},
-    addToReadingList:(data)=>{dispatch({type:"ADD_TO_READING_LIST",data})}
+    addToReadingList: (articleInfo,country_id) => {dispatch(addingToReadingList(articleInfo,country_id))}
   }
 }
 
